220383ee3fa1de9924123c1d4230814acfc18a2f
Umar Ibrahim Muh'd <65617343+umaribrahimkambiya@users.noreply.github.com>
2026-07-21 16:52:57 +0530

    Add files via upload

---
+++ codes/prediction_count_vectorizer.py
@@ -0,0 +1,42 @@
+"""
+ # @author rajchoudhary
+ # @email raj.choudhary1603@gmail.com
+ # @create date 2019-03-18 01:26:48
+ # @modify date 2019-04-09 01:41:53
+ # @desc [File for predicting the label of the news entered by the user.]
+"""
+
+# Importing the libraries
+import os
+import pickle
+
+
+user_input = input('Enter the news to verify:\t')
+print('The news entered:\t{}'.format(user_input))
+
+
+# Function for making the prediction
+def make_prediction(user_input):
+    """Function to load the desired model and make the prediction using
+    the model and displaying the label to the user.
+
+    Parameters:
+    -----------
+    user_input: string
+        The news the user wants to confirm.
+    """
+    # Loading the desired model
+    final_model = pickle.load(
+        open(os.path.join('../models', 
+            'voting_classifier_count_vectorizer.pkl'), 'rb'), 
+    )
+
+    # Making prediction on the user_input and displaying the result
+    prediction = final_model.predict([user_input])
+    prediction_probability = final_model.predict_proba([user_input])
+    print('Predicted label:\t{}'.format(prediction))
+    print('Truth probability:\t{}'.format(prediction_probability[0][1]))
+
+
+if __name__ == '__main__':
+    make_prediction(user_input)
